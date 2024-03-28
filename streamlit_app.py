9558f537b6f4f65b8be354965b36786ccb61335e
access2sumit <access2sumit@gmail.com>
2024-03-28 00:12:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import panadas as pd
 
 #from snowflake.snowpark.context import get_active_session
 cnx = st.connection("snowflake")
@@ -26,6 +27,10 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUI
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
+
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients',my_dataframe,max_selections=5)
 if ingredients_list:
@@ -34,6 +39,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Value')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen )
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
